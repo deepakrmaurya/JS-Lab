8e459e5063305b5d39b75d98037a1068d4f5ffda
Deepak Maurya <deepakrmaurya@outlook.com>
2025-08-13 20:44:46 +0530

    some code

---
+++ NamasteJs/scripts/app.js
@@ -0,0 +1,33 @@
+//Callback Function Example
+// setTimeout(function () {
+//   console.log("Timer");
+// }, 5000);
+
+// function x(y) {
+//   console.log("x");
+//   y();
+// }
+
+// x(function y() {
+//   console.log("y");
+// });
+
+//This code runs fine, but what if we print num and getName before initialzing
+// var num = 6;
+
+// function getName() {
+//   console.log("Hello There, this is getName function");
+// }
+
+// getName();
+// console.log(num);
+
+// This will still print the function console log but num is undefined
+// getName();
+// console.log(num);
+
+// var num = 6;
+
+// function getName() {
+//   console.log("Hello There, this is getName function");
+// }
